d2cb39e77d6131e530e1ecbb5e9bbfd16f697f92
manhjjjjj <pubbg21@gmail.com>
2023-03-09 20:03:56 +0700

    feature/#539/homepage_task

---
+++ my-app/src/components/Home/CTA.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+export default function CTA() {
+    return (
+        <div className="CTA">
+            <div className="title1-cta">
+                <p>Stay in the loop on CryptoArt and NFTs</p>
+            </div>
+            <div className="title2-cta">
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+                    dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+                </p>
+            </div>
+            <div className="ipt-cta">
+                <input type="text" placeholder="You Email" />
+                <button>Subscribe</button>
+            </div>
+        </div>
+    )
+}
